371e9c98378825b8a7ff01ec893de65c8966caa5
ChestnutLord <134998927+ChestnutLord@users.noreply.github.com>
2025-02-25 23:04:41 +0300

    Паттерн Prototype

---
+++ src/Prototype/Demo.java
@@ -0,0 +1,14 @@
+package Prototype;
+
+public class Demo {
+    public static void main(String[] args) {
+        Prototype prototype = new MyObject("Èìÿ", "Yaaew_3-1-uy_eR4", 86, true);
+        Prototype prototype2=prototype.clone();
+        Prototype prototype3=prototype.clone();
+        Prototype prototype4=prototype.clone();
+
+        System.out.println(prototype2);
+        System.out.println(prototype3);
+        System.out.println(prototype4);
+    }
+}
