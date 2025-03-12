56b5bae8621a7aa4a82f268b42534ec30085599e
Dmitriy Gvozdenko <textdmitriy@gmail.com>
2025-03-12 13:45:58 +0300

    Паттрен Прокси- doing

---
+++ src/Structural/Proxy/some_cool_media_library/Video.java
@@ -0,0 +1,14 @@
+package Structural.Proxy.some_cool_media_library;
+
+public class Video {
+
+    private String id;
+    private String title;
+    private String data;
+
+    public Video(String id, String title) {
+        this.id = id;
+        this.title = title;
+        this.data = "Random data";
+    }
+}
